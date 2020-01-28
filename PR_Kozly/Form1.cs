030dc8dc59573544190961d2fb18a6205f083e50
dawid.szpilewski <dawid.szpilewski@DAWSZP>
2020-01-28 14:53:50 +0100

    Dodaj pliki projektów.

---
+++ PR_Kozly/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms; 
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.Operations;
+ 
+namespace PR_Kozly
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Model model= new Model();
+            if (!model.GetConnectionStatus())
+            {
+                MessageBox.Show("Tekla nie uruchomiona");
+                return;
+            }
+
+            ModelInfo modelInfo = model.GetInfo();
+            string name = modelInfo.ModelName;
+            MessageBox.Show(string.Format("Nazwa modelu: {0}", name));
+            Operation.DisplayPrompt(string.Format("Bieżący model: {0}", name));
+        }
+    }
+}
